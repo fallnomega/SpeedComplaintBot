8fe45707c678e5ad26bc3f90cdf776eb58c44158
fallnomega <fallnomega@gmail.com>
2023-03-24 15:26:32 -0700

    laying some of the groundwork for the speed testing

---
+++ TwitterBot.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import os
+
+TWITTER_EMAIL = os.environ.get('T_USER')
+TWITTER_PASSWORD = os.environ.get('T_PASS')
+PROMISED_DOWN = 150
+PROMISED_DOWN = 10
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        chrome_driver = 'chromedriverDirectory'
+        self.CHROME_DRIVER_PATH = chrome_driver
+    def up(self):
+        isp = 'https://www.sonic.com/speedtest'
+        return
+
+    def down(self):
+        return
+
+    def get_internet_speed(self):
+        return
+
+    def tweet_at_provider(self):
+        return
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import re
+# options = Options()
+# options.add_experimental_option("detach", True)
+# chrome_driver = 'chromedriverDirectory'
+# driver = webdriver.Chrome(executable_path=chrome_driver, options=options)
+
+
